df0000cf2fe46a652187d33e2bcb054f1c205ba2
Aisezima <djakenova2004@gmail.com>
2023-01-17 17:11:39 +0600

    searchPlayerPage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import RegisPage from '../src/components/Header/RegisPage'
+import Header from '../src/components/Header/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <RegisPage/>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
